dc7cd5f3764821ac1a40d76d051d1bfeddedceb0
Akhya04 <akhyarastogi04@gmail.com>
2026-04-28 15:42:35 +0530

    added department repo and department entity

---
+++ src/main/java/com/example/HR_Management/entity/Department.java
@@ -0,0 +1,55 @@
+package com.example.HR_Management.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @Column(name = "department_id", precision = 4, scale = 0)
+    private BigDecimal departmentId;
+
+    @Column(name = "department_name", nullable = false, length = 30)
+    private String departmentName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id")
+    private Employee manager;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+
+    @OneToMany(mappedBy = "department")
+    private List<JobHistory> jobHistories;
+
+    public Department() {}
+
+    public Department(BigDecimal departmentId, String departmentName) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+    }
+
+    public BigDecimal getDepartmentId() { return departmentId; }
+    public void setDepartmentId(BigDecimal departmentId) { this.departmentId = departmentId; }
+
+    public String getDepartmentName() { return departmentName; }
+    public void setDepartmentName(String departmentName) { this.departmentName = departmentName; }
+
+    public Location getLocation() { return location; }
+    public void setLocation(Location location) { this.location = location; }
+
+    public Employee getManager() { return manager; }
+    public void setManager(Employee manager) { this.manager = manager; }
+
+    public List<Employee> getEmployees() { return employees; }
+    public void setEmployees(List<Employee> employees) { this.employees = employees; }
+
+    
+}
