7df4e6874d0f915b37892de8965055442cd8f11b
Akhya04 <akhyarastogi04@gmail.com>
2026-04-29 14:50:30 +0530

    Validation added

--- src/main/java/com/example/HR_Management/entity/Department.java
+++ src/main/java/com/example/HR_Management/entity/Department.java
@@ -1,6 +1,11 @@
 package com.example.HR_Management.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,18 +15,26 @@ import java.util.List;
 public class Department {
 
     @Id
+    @NotNull(message = "Department ID is mandatory")  
     @Column(name = "department_id", precision = 4, scale = 0) 
     private BigDecimal departmentId;
-
+    
+    @NotBlank(message = "Department name is mandatory")
+    @Size(max = 30, message = "Name cannot exceed 30 characters")
+    @Pattern(regexp = "^[^0-9]*$", message = "Department name cannot contain numbers")
     @Column(name = "department_name", nullable = false, length = 30) 
     private String departmentName;
 
+    @NotNull(message = "Location is mandatory")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "location_id") 
     private Location location;
 
+    
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "manager_id") 
+    @JoinColumn(name = "manager_id", 
+            referencedColumnName = "employee_id",
+            columnDefinition = "DECIMAL(6,0)") 
     private Employee manager;
 
     @OneToMany(mappedBy = "department")
